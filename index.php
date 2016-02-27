de3fdceb86326b5f1f68d33d7ffcc9754ae5d452
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-02-27 11:03:06 -0300

    att

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
  <TITLE>Você Prefeito!</TITLE>
 </HEAD>
 <BODY>
-      <img src="C:\Users\Vainer\Documents\sistema\img" border="0">
+      <img src="\img\emconstrucao.png" border="0">
 </BODY>
 </HTML>
