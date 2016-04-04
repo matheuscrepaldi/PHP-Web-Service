c7818ec232d22dabc5924116a5e41df2174f1cc1
vcprefeito <contato@vcprefeito.com.br>
2016-04-04 18:24:08 -0300

    df
    dfg

--- conexao.php
+++ conexao.php
@@ -2,16 +2,16 @@
 
 	$conexao = mysqli_connect("mysql.hostinger.com.br", "u633448963_root", "masterkey") or die ("Erro: " . mysqli_error());
 	
-	$base = mysqli_select_db("u633448963_tcc") or die ("Erro: " . mysqli_error());
+	$base = mysqli_select_db("u633448963_tcc", $conexao) or die ("Erro: " . mysqli_error());
 	
 	
-	$sql = "select * from Usuario";
+	/*$sql = "select * from Usuario";
 	
 	$res = mysql_query($sql);
 	
 	$reg = mysql_fetch_array($res);
 	
-	echo "Nome do usuario: " . $reg['nome_usu'];
+	echo "Nome do usuario: " . $reg['nome_usu'];*/
 	
 	
 
