3fb34fd0f30cb14049157d5550868ec203778aaf
vcprefeito <contato@vcprefeito.com.br>
2016-04-04 18:25:44 -0300

    asd
    asd

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 
 	$conexao = mysqli_connect("mysql.hostinger.com.br", "u633448963_root", "masterkey") or die ("Erro: " . mysqli_error());
 	
-	$base = mysqli_select_db("u633448963_tcc", $conexao) or die ("Erro: " . mysqli_error());
+	$base = mysqli_select_db($conexao, "u633448963_tcc") or die ("Erro: " . mysqli_error());
 	
 	
 	/*$sql = "select * from Usuario";
