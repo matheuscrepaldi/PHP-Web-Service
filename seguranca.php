159a159119ba94bb02f17cec8dc3a88dec7a84e8
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-05-08 19:08:57 -0300

    att

--- seguranca.php
+++ seguranca.php
@@ -29,7 +29,7 @@ $_SG['tabela'] = 'usuarios';       // Nome da tabela onde os usuários são salv
 // ======================================
 // Verifica se precisa fazer a conexão com o MySQL
 if ($_SG['conectaServidor'] == true) {
-  $_SG['link'] = mysql_connect($_SG['servidor'], $_SG['usuario'], $_SG['senha']) or die("MySQL: Não foi possível conectar-se ao servidor [".$_SG['servidor']."].");
+  $_SG['link'] = mysqli_connect($_SG['servidor'], $_SG['usuario'], $_SG['senha']) or die("MySQL: Não foi possível conectar-se ao servidor [".$_SG['servidor']."].");
   mysql_select_db($_SG['banco'], $_SG['link']) or die("MySQL: Não foi possível conectar-se ao banco de dados [".$_SG['banco']."].");
 }
 // Verifica se precisa iniciar a sessão
