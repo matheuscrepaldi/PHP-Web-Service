19c5df11685aadb215f9b98862cf0e701e63252a
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-05-13 09:51:46 -0300

    Novo Login

---
+++ dbconfig.php
@@ -0,0 +1,27 @@
+<?php
+class Database
+{   
+    private $host = "mysql.hostinger.com.br";
+    private $db_name = "u633448963_root";
+    private $username = "root";
+    private $password = "123456";
+    public $conn;
+     
+    public function dbConnection()
+	{
+     
+	    $this->conn = null;    
+        try
+		{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+        }
+		catch(PDOException $exception)
+		{
+            echo "Connection error: " . $exception->getMessage();
+        }
+         
+        return $this->conn;
+    }
+}
+?>
