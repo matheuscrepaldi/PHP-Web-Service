9708629d3c1149503355fe5bab2ec42c5033ba68
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-05-31 08:56:39 -0300

    correção

--- home.php
+++ home.php
@@ -1,96 +1,40 @@
 <?php
+include_once 'dbconfig.php';
 
-	require_once("session.php");
-	
-	require_once("class.user.php");
-	$auth_user = new USER();
-	
-	
-	$user_id = $_SESSION['user_session'];
-	
-	$stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
-	$stmt->execute(array(":user_id"=>$user_id));
-	
-	$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+if(!$user->is_loggedin())
+{
+ $user->redirect('login.php');
+}
 
+$user_id = $_SESSION['user_session'];
+
+$stmt = $DB_con->prepare("SELECT * FROM users WHERE user_id=:user_id");
+
+$stmt->execute(array(":user_id"=>$user_id));
+
+$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
-<script type="text/javascript" src="jquery-1.11.3-jquery.min.js"></script>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css"  />
 <link rel="stylesheet" href="style.css" type="text/css"  />
-<title>welcome - <?php print($userRow['user_email']); ?></title>
+<title>Bem vindo - <?php print($userRow['user_name']); ?></title>
 </head>
 
 <body>
 
-<nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Home</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Sobre</a></li>
-            <li><a href="#">Denúncias</a></li>
-            <li><a href="#">Consultas</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-			  <span class="glyphicon glyphicon-user"></span>&nbsp;Olá' <?php echo $userRow['user_name']; ?>&nbsp;<span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span>&nbsp;View Profile</a></li>
-                <li><a href="logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sair</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+<div class="header">
 
-
-    <div class="clearfix"></div>
-    	
-    
-<div class="container-fluid" style="margin-top:80px;">
-	
-    <div class="container">
-    
-    	<label class="h5">Bem vindo : <?php print($userRow['user_name']); ?></label>
-        <hr />
-        
-        <h1>
-        <a href="home.php"><span class="glyphicon glyphicon-home"></span> home</a> &nbsp; 
-        <a href="profile.php"><span class="glyphicon glyphicon-user"></span> profile</a></h1>
-       	<hr />
-        
-        <p class="h4">User Home Page</p> 
-       
-        
-    <p class="blockquote-reverse" style="margin-top:200px;">
-    
-    </p>
-
-    <p class="blockquote-reverse" style="margin-top:200px;">
-    vcprefeito.com.br - 2016<br /><br />
-    </p>
-    
+    <div class="right">
+     <label><a href="logout.php?logout=true"><i class="glyphicon glyphicon-log-out"></i> logout</a></label>
     </div>
 
 </div>
-
-<script src="bootstrap/js/bootstrap.min.js"></script>
-
+<div class="content">
+Bem vindo : <?php print($userRow['user_name']); ?>
+</div>
 </body>
 </html>
