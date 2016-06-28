5e8f91712dd7ed504a9efca96957024fb2946f74
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-06-28 09:00:40 -0300

    teste

--- login.php
+++ login.php
@@ -5,42 +5,55 @@ $login = new USER();
 
 if($login->is_loggedin()!="")
 {
-	$login->redirect('home.php');
+  $login->redirect('home.php');
 }
 
 if(isset($_POST['btn-login']))
 {
-	$uname = strip_tags($_POST['txt_uname_email']);
-	$umail = strip_tags($_POST['txt_uname_email']);
-	$upass = strip_tags($_POST['txt_password']);
-		
-	if($login->doLogin($uname,$umail,$upass))
-	{
-		$login->redirect('home.php');
-	}
-	else
-	{
-		$error = "Wrong Details !";
-	}	
+  $uname = strip_tags($_POST['txt_uname_email']);
+  $umail = strip_tags($_POST['txt_uname_email']);
+  $upass = strip_tags($_POST['txt_password']);
+    
+  if($login->doLogin($uname,$umail,$upass))
+  {
+    $login->redirect('home.php');
+  }
+  else
+  {
+    $error = "Dados Incorretos!";
+  } 
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Coding Cage : Login</title>
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
-<link rel="stylesheet" href="style.css" type="text/css"  />
-</head>
-<body>
 
-<div class="signin-form">
+<!DOCTYPE html>
 
-	<div class="container">
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Log in</title>
+    
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    
+    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+    
+    <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
+
+  </head>
+  <body>
+
+  	<div class="signin-form">
+
+		<div class="container">
      
         
-       <form class="form-signin" method="post" id="login-form">
+       	<form class="form-signin" method="post" id="login-form">
       
         <h2 class="form-signin-heading">Log In to WebApp.</h2><hr />
         
@@ -58,28 +71,28 @@ if(isset($_POST['btn-login']))
         </div>
         
         <div class="form-group">
-        <input type="text" class="form-control" name="txt_uname_email" placeholder="Username or E mail ID" required />
+        <input type="text" class="form-control" name="txt_uname_email" placeholder="Usuáio ou Email" required />
         <span id="check-e"></span>
         </div>
         
         <div class="form-group">
-        <input type="password" class="form-control" name="txt_password" placeholder="Your Password" />
+        <input type="password" class="form-control" name="txt_password" placeholder="Senha" />
         </div>
        
      	<hr />
         
         <div class="form-group">
             <button type="submit" name="btn-login" class="btn btn-default">
-                	<i class="glyphicon glyphicon-log-in"></i> &nbsp; SIGN IN
+                	<i class="glyphicon glyphicon-log-in"></i> &nbsp; Entrar
             </button>
         </div>  
       	<br />
-            <label>Don't have account yet ! <a href="sign-up.php">Sign Up</a></label>
+            <label>Ainda não possui uma conta?<a href="sign-up.php">Registrar-se</a></label>
       </form>
 
     </div>
     
 </div>
-
-</body>
+ 
+ </body>
 </html>
