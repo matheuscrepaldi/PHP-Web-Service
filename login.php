6f3f92340473b1c5206ad4e4ab18baa72db98d49
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-06-28 09:32:29 -0300

    adas

--- login.php
+++ login.php
@@ -1,132 +1,85 @@
 <?php
-	session_start();
-	require_once("class.user.php");
-	$login = new USER();
+session_start();
+require_once("class.user.php");
+$login = new USER();
 
-	if($login->is_loggedin()!="")
-	{
-  		$login->redirect('home.php');
-	}
+if($login->is_loggedin()!="")
+{
+	$login->redirect('home.php');
+}
 
 if(isset($_POST['btn-login']))
 {
-  $uname = strip_tags($_POST['txt_uname_email']);
-  $umail = strip_tags($_POST['txt_uname_email']);
-  $upass = strip_tags($_POST['txt_password']);
-    
-  if($login->doLogin($uname,$umail,$upass))
-  {
-    $login->redirect('home.php');
-  }
-  else
-  {
-    $error = "Dados Incorretos!";
-  } 
+	$uname = strip_tags($_POST['txt_uname_email']);
+	$umail = strip_tags($_POST['txt_uname_email']);
+	$upass = strip_tags($_POST['txt_password']);
+		
+	if($login->doLogin($uname,$umail,$upass))
+	{
+		$login->redirect('home.php');
+	}
+	else
+	{
+		$error = "Wrong Details !";
+	}	
 }
 ?>
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Log in</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="../../plugins/iCheck/square/blue.css">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Coding Cage : Login</title>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+<link rel="stylesheet" href="style.css" type="text/css"  />
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="login-box-body">
-    <p class="login-box-msg">Sign in to start your session</p>
+<body>
 
-    <form class="form-signin" method="post" id="login-form">
-      <div class="form-group has-feedback">
+<div class="signin-form">
 
-      <div id="error">
+	<div class="container">
+     
+        
+       <form class="form-signin" method="post" id="login-form">
+      
+        <h2 class="form-signin-heading">Log In to WebApp.</h2><hr />
+        
+        <div id="error">
         <?php
 			if(isset($error))
-			 {
-		?>
+			{
+				?>
                 <div class="alert alert-danger">
                    <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?> !
                 </div>
-        <?php
-			 }
+                <?php
+			}
 		?>
         </div>
-
-        <input type="email" class="form-control" name="txt_uname_email" placeholder="Usuáio ou Email" required>
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" class="form-control" name="txt_password" placeholder="Senha">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Manter conectado
-            </label>
-          </div>
+        
+        <div class="form-group">
+        <input type="text" class="form-control" name="txt_uname_email" placeholder="Username or E mail ID" required />
+        <span id="check-e"></span>
         </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
+        
+        <div class="form-group">
+        <input type="password" class="form-control" name="txt_password" placeholder="Your Password" />
         </div>
-        <!-- /.col -->
-      </div>
-    </form>
+       
+     	<hr />
+        
+        <div class="form-group">
+            <button type="submit" name="btn-login" class="btn btn-default">
+                	<i class="glyphicon glyphicon-log-in"></i> &nbsp; SIGN IN
+            </button>
+        </div>  
+      	<br />
+            <label>Don't have account yet ! <a href="sign-up.php">Sign Up</a></label>
+      </form>
 
-    <div class="social-auth-links text-center">
-      <p>- OR -</p>
-      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-        Facebook</a>
     </div>
-    <!-- /.social-auth-links -->
-
-    <a href="#">I forgot my password</a><br>
-    <a href="sign-up.php" class="text-center">Registrar-se</a>
-
-  </div>
-  <!-- /.login-box-body -->
+    
 </div>
-<!-- /.login-box -->
 
-<!-- jQuery 2.2.0 -->
-<script src="../../plugins/jQuery/jQuery-2.2.0.min.js"></script>
-<!-- Bootstrap 3.3.6 -->
-<script src="../../bootstrap/js/bootstrap.min.js"></script>
-<!-- iCheck -->
-<script src="../../plugins/iCheck/icheck.min.js"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
-    });
-  });
-</script>
 </body>
 </html>
