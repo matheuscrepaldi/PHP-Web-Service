dd87d4e8a9acd066f97d4bd46adbb5b3b971bf67
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-06-28 09:20:39 -0300

    teste

--- login.php
+++ login.php
@@ -31,7 +31,7 @@ if(isset($_POST['btn-login']))
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Log in</title>
+    <title>Log in - VcPrefeito</title>
     
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     
@@ -46,53 +46,70 @@ if(isset($_POST['btn-login']))
     <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
 
   </head>
-  <body>
+  <body class="hold-transition login-page">
 
   	<div class="signin-form">
 
 		<div class="container">
-     
-        
-       	<form class="form-signin" method="post" id="login-form">
-      
-        <h2 class="form-signin-heading">Log In to WebApp.</h2><hr />
-        
-        <div id="error">
-        <?php
-			if(isset($error))
-			{
-				?>
-                <div class="alert alert-danger">
-                   <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?> !
-                </div>
-                <?php
-			}
-		?>
-        </div>
-        
-        <div class="form-group">
-        <input type="text" class="form-control" name="txt_uname_email" placeholder="Usuáio ou Email" required />
-        <span id="check-e"></span>
-        </div>
-        
-        <div class="form-group">
-        <input type="password" class="form-control" name="txt_password" placeholder="Senha" />
-        </div>
-       
-     	<hr />
-        
-        <div class="form-group">
-            <button type="submit" name="btn-login" class="btn btn-default">
-                	<i class="glyphicon glyphicon-log-in"></i> &nbsp; Entrar
-            </button>
-        </div>  
-      	<br />
-            <label>Ainda não possui uma conta?<a href="sign-up.php">Registrar-se</a></label>
-      </form>
+     	
+     	<div class="login-logo">
+   			 <a href="../../index2.html"><b>Admin</b>LTE</a>
+  		</div>
 
+      	<div class="login-box-body">
+    		<p class="login-box-msg">Entre para realizar uma denúncia</p>
+
+ 			   <form class="form-signin" method="post" id="login-form">
+      			<div class="form-group has-feedback">
+        			<input type="email" class="form-control" placeholder="Email">
+        			<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      			</div>
+
+      	<div class="form-group has-feedback">
+        	<input type="password" class="form-control" placeholder="Senha">
+        	<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      	</div>
+
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Manter-se conectado
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+
+    <div class="social-auth-links text-center">
+      <p>- OR -</p>
+      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i>Entrar usando Facebook</a>
     </div>
-    
-</div>
- 
+    <!-- /.social-auth-links -->
+
+    <a href="#">Esqueci minha senha</a><br>
+    <a href="register.html" class="text-center">Registrar-se</a>
+
+  </div>
+ 		<script src="../../plugins/jQuery/jQuery-2.2.0.min.js"></script>
+		<!-- Bootstrap 3.3.6 -->
+		<script src="../../bootstrap/js/bootstrap.min.js"></script>
+		<!-- iCheck -->
+		<script src="../../plugins/iCheck/icheck.min.js"></script>	
+	<script>
+ 	 $(function () {
+    	$('input').iCheck({
+      	checkboxClass: 'icheckbox_square-blue',
+      	radioClass: 'iradio_square-blue',
+      	increaseArea: '20%' // optional
+    	});
+  	});
+
+	</script>
  </body>
 </html>
