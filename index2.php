1b0f0a4e809e8776e90c36a40e333233643bf620
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-07-04 10:24:58 -0300

    asd

--- index2.php
+++ index2.php
@@ -288,7 +288,7 @@
               <!-- Menu Footer-->
               <li class="user-footer">
                 <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
+                  <a href="#" class="btn btn-default btn-flat">Perfil</a>
                 </div>
                 <div class="pull-right">
                   <a href="logout.php?logout=true" class="btn btn-default btn-flat">Sair</a>
