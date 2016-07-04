79e23700f94fbb7bb8396ad68e895d6ab0efa0c6
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-07-04 14:58:57 -0300

    changes

--- index2.php
+++ index2.php
@@ -93,8 +93,31 @@
 
     </nav>
   </header>
-  <!-- Left side column. contains the logo and sidebar -->
- 
+
+  <!-- Left side column. contains the sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p> &nbsp;<?php echo $userRow['user_name']; ?></p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MAIN NAVIGATION</li>
+      </ul>
+      </section>
+      </aside>
   
   <style>
    		.Flexible-container {
