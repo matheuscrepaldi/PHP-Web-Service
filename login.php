f28ceb47677d71e763f9e1167c9a536d966c146c
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-07-04 15:50:57 -0300

    login

--- login.php
+++ login.php
@@ -23,6 +23,68 @@ if(isset($_POST['btn-login']))
 		$error = "Wrong Details !";
 	}	
 }
+
+  //login FACEBOOK
+session_start();
+if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['code'])){
+
+  // Informe o seu App ID abaixo
+  $appId = '1233962869948144';
+
+  // Digite o App Secret do seu aplicativo abaixo:
+  $appSecret = '0e6d3ce6463ddc3a3b33d496be1fcda3';
+
+  // Url informada no campo "Site URL"
+  $redirectUri = urlencode('http://vcprefeito.com.br/login.php/');
+
+  // Obtém o código da query string
+  $code = $_GET['code'];
+
+  // Monta a url para obter o token de acesso e assim obter os dados do usuário
+  $token_url = "https://graph.facebook.com/oauth/access_token?"
+  . "client_id=" . $appId . "&redirect_uri=" . $redirectUri
+  . "&client_secret=" . $appSecret . "&code=" . $code;
+
+  //pega os dados
+  $response = @file_get_contents($token_url);
+  if($response){
+    $params = null;
+    parse_str($response, $params);
+    if(isset($params['access_token']) && $params['access_token']){
+      $graph_url = "https://graph.facebook.com/me?access_token="
+      . $params['access_token'];
+      $user = json_decode(file_get_contents($graph_url));
+
+  // nesse IF verificamos se veio os dados corretamente
+      if(isset($user->email) && $user->email){
+
+  /*
+  *Apartir daqui, você já tem acesso aos dados usuario, podendo armazená-los
+  *em sessão, cookie ou já pode inserir em seu banco de dados para efetuar
+  *autenticação.
+  *No meu caso, solicitei todos os dados abaixo e guardei em sessões.
+  */
+
+        $_SESSION['email'] = $user->email;
+        $_SESSION['nome'] = $user->name;
+        $_SESSION['localizacao'] = $user->location->name;
+        $_SESSION['uid_facebook'] = $user->id;
+        $_SESSION['user_facebook'] = $user->username;
+        $_SESSION['link_facebook'] = $user->link;
+      }
+    }else{
+      echo "Erro de conexão com Facebook";
+      exit(0);
+    }
+
+  }else{
+    echo "Erro de conexão com Facebook";
+    exit(0);
+  }
+}else if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['error'])){
+  echo 'Permissão não concedida';
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -86,7 +148,7 @@ if(isset($_POST['btn-login']))
         </div> 
         <div class="social-auth-links text-center">
       <p>- OU -</p>
-      <a href="https://www.facebook.com/dialog/oauth?client_id=YOUR_APP_ID&redirect_uri=YOUR_REDIRECT_URI&scope=COMMA_SEPARATED_LIST_OF_PERMISSION_NAMES" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Logar pelo Facebook</a>
+      <a href="https://www.facebook.com/dialog/oauth?client_id=1233962869948144&redirect_uri=http://vcprefeito.com.br/login.php&scope=COMMA_SEPARATED_LIST_OF_PERMISSION_NAMES" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Logar pelo Facebook</a>
     </div>
       	<br />
             <label>Não possui uma conta?<a href="sign-up.php"> Registrar-se</a></label>
