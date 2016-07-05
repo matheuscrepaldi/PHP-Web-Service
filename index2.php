074c318bd2fd5c43e9b23563cc3ef388e1688e79
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-07-05 15:48:38 -0300

    teste

--- index2.php
+++ index2.php
@@ -1,21 +1,3 @@
-<?php
-
-  require_once("session.php");
-
-  require_once("fbconfig.php");
-  
-  require_once("class.user.php");
-  $auth_user = new USER();
-  
-  
-  $user_id = $_SESSION['user_session'];
-  
-  $stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
-  $stmt->execute(array(":user_id"=>$user_id));
-  
-  $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
-
-?>
 
 <!DOCTYPE html>
 <html>
