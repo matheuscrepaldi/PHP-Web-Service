2d8bccb491cd8ae60908ea7c30d15d2318ddc013
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-07-06 16:52:49 -0300

    layout

--- sign-up.php
+++ sign-up.php
@@ -120,7 +120,7 @@ if(isset($_POST['btn-signup']))
         		<input type="password" class="form-control" name="txt_upass" placeholder="Senha">
         		<span class="glyphicon glyphicon-lock form-control-feedback"></span>
       		</div>
-      		
+
             <div class="clearfix"></div><hr />
             <div class="form-group">
             	<button type="submit" class="btn btn-primary" name="btn-signup">
@@ -134,6 +134,21 @@ if(isset($_POST['btn-signup']))
 </div>
 
 </div>
+
+<script src="../../plugins/jQuery/jQuery-2.2.0.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="../../bootstrap/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="../../plugins/iCheck/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
 
 </body>
 </html>
