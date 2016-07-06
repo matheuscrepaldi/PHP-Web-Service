238aabae862f8bad62328f10bb9afbbc2b7bee28
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-07-06 15:25:13 -0300

    capirotooooooo

--- index2.php
+++ index2.php
@@ -120,7 +120,7 @@
             <i class="fa fa-dashboard"></i> <span>Denúncias</span> <i class="fa fa-angle-left pull-right"></i>
           </a>
           <ul class="treeview-menu">
-            <li><a href="" onclick="Denuncias();"><i class="fa fa-circle-o"></i> Realizar Denúncia</a></li>
+            <li><a href="" onclick="return chamarPhpAjax();"><i class="fa fa-circle-o"></i> Realizar Denúncia</a></li>
             <li><a href="#"><i class="fa fa-circle-o"></i> Consultar</a></li>
           </ul>
         </li>
@@ -157,13 +157,23 @@
       <div class="row">
         <div class="col-md-12">
 			<div  class="Flexible-container">
-				
+				<iframe  src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d14859.019772468268!2d-50.477355349999996!3d-21.39955165!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1463764622779" width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
 
         <script>
-          function Denuncias(){
-            <?php include("Teste.html");?>
-          }
-        </script>
+          function chamarPhpAjax() {
+          $.ajax({
+          url:'Teste.html',
+          complete: function (response) {
+          alert(return url:'Teste.html');
+        },
+          error: function () {
+          alert('Erro');
+      }
+  });  
+
+  return false;
+}
+</script>
 			</div>
             </div>
             <!-- /.info-box-content -->
