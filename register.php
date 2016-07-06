d85dab556cce62cd395990b6f8c1d1b02abc2297
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-07-06 16:42:37 -0300

    teste layout

--- register.php
+++ register.php
@@ -115,15 +115,15 @@ if(isset($_POST['btn-signup']))
       ?>
 
       <div class="form-group has-feedback">
-        <input type="text" class="form-control" placeholder="Nome de usuário" value="<?php if(isset($error)){echo $uname;}?>" />
+        <input type="text" class="form-control" name="txt_uname" placeholder="Nome de usuário" value="<?php if(isset($error)){echo $uname;}?>" />
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email" value="<?php if(isset($error)){echo $umail;}?>" />
+        <input type="email" class="form-control" name="txt_umail" placeholder="Email" value="<?php if(isset($error)){echo $umail;}?>" />
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Senha">
+        <input type="password" class="form-control" name="txt_upass" placeholder="Senha">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
 
