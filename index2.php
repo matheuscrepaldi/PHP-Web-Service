6d03b4e673304b26e1f2b165891941079c351a9c
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-07-06 16:19:57 -0300

    rollback

--- index2.php
+++ index2.php
@@ -157,8 +157,7 @@
       <div class="row">
         <div class="col-md-12">
 			<div  class="Flexible-container">
-				
-        <li><a href="Teste.html"><i class="fa fa-circle-o"></i> Realizar Denúncia</a></li>
+				<iframe  src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d14859.019772468268!2d-50.477355349999996!3d-21.39955165!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1463764622779" width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
 			</div>
             </div>
             <!-- /.info-box-content -->
