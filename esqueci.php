87c119827164ff0eb54c3306dbf23363d6d1c4e6
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-07-22 15:46:17 -0300

    ad

--- esqueci.php
+++ esqueci.php
@@ -1,3 +1,19 @@
+<?php
+  //1 – Definimos Para quem vai ser enviado o email
+  $para = "vainer_fabri@hotmail.com";
+  
+//5 – agora inserimos as codificações corretas e  tudo mais.
+  $headers =  "Content-Type:text/html; charset=UTF-8\n";
+  $headers .= "From:  vcprefeito.com.br<sysadmin@vcprefeito.com.br>\n"; //Vai ser //mostrado que  o email partiu deste email e seguido do nome
+  $headers .= "X-Sender:  <sysadmin@vcprefeito.com.br>\n"; //email do servidor //que enviou
+  $headers .= "X-Mailer: PHP  v".phpversion()."\n";
+  $headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
+  $headers .= "Return-Path:  <sysadmin@vcprefeito.com.br>\n"; //caso a msg //seja respondida vai para  este email.
+  $headers .= "MIME-Version: 1.0\n";
+
+		mail($para, $headers);  //função que faz o envio do email.
+  ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -13,16 +29,38 @@
 
 <div class="container">
     	
-        <form action="" method="post" class="form-signin" enctype="multipart/form-data">
+        <form method="post" class="form-signin">
             <h2 class="form-signin-heading">Redefinir senha.</h2><hr />
-            
+            <?php
+			if(isset($error))
+			{
+			 	foreach($error as $error)
+			 	{
+					 ?>
+                     <div class="alert alert-danger">
+                        <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?>
+                     </div>
+                     <?php
+				}
+			}
+			else if(isset($_GET['joined']))
+			{
+				 ?>
+                 <div class="alert alert-info">
+                      <i class="glyphicon glyphicon-log-in"></i> &nbsp; Email enviado! <a href='index.php'>Entrar</a> aqui
+                 </div>
+                 <?php
+			}
+			?>
             <div class="form-group">
             	<input type="text" class="form-control" name="txt_upass" placeholder="Email para redefinição" />
             </div>
 
             <div class="clearfix"></div><hr />
             <div class="form-group">
-            	<input type="submit" class="button btn btn-primary btn-large" name="recuperar" value="Recuperar Senha">
+            	<button type="submit" class="btn btn-primary" name="btn-signup">
+                	<i class="glyphicon glyphicon-open-file"></i>&nbsp;Enviar
+                </button>
             </div>
             <br />
             <label>Já possui uma conta? <a href="index.php">Entrar</a></label>
@@ -31,5 +69,6 @@
 </div>
 
 </div>
+
 </body>
 </html>
