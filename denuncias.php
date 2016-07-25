2d25c12733dd53f0eb03b9c81a76dc2c304f5077
vcprefeito <contato@vcprefeito.com.br>
2016-07-25 11:16:51 -0300

    erro
    erro

--- denuncias.php
+++ denuncias.php
@@ -42,7 +42,7 @@
 		try
 		{
 			$atualiza = $auth_denuncia->runQuery("SELECT data_denuncia, assunto_denuncia FROM denuncias WHERE data_denuncia=:udata OR assunto_denuncia=:uassunto");
-			$atualiza->execute(array(':udata'=>$udata, ':uassunto'=>$uassunto, ':udescricao'=>:$udescricao));
+			$atualiza->execute(array(':udata'=>$udata, ':uassunto'=>$uassunto, ':udescricao'=>$udescricao));
 			$row=$atualiza->fetch(PDO::FETCH_ASSOC);
 
 			if($auth_denuncia->register($udata, $uassunto, $udescricao)){
