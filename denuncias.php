1d555ee5933d85d964547972db799c1e98d8d195
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-08-05 19:13:33 -0300

    teste banco

--- denuncias.php
+++ denuncias.php
@@ -3,7 +3,6 @@
   require_once("session.php");
   
   require_once("class.user.php");
-
   require_once("class.denuncia.php");
 
   $auth_user = new USER();
@@ -18,7 +17,6 @@
   $atualiza->execute(array(":id_denuncia"=>$id_denuncia));
   
   $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
-
   $denunciaRow=$atualiza->fetch(PDO::FETCH_ASSOC);
 
   if(isset($_POST['btn-salvar']))
@@ -41,14 +39,9 @@
 	{
 		try
 		{
-			$atualiza = $auth_denuncia->runQuery("SELECT data_denuncia, assunto_denuncia, descricao FROM denuncias WHERE data_denuncia=:udata OR assunto_denuncia=:uassunto OR descricao=:udescricao");
-			
-			$atualiza->execute(array(':udata'=>$udata, ':uassunto'=>$uassunto, ':udescricao'=>$udescricao));
+			$atualiza = $denuncia->runQuery("SELECT data_denuncia, assunto_denuncia FROM denuncias WHERE data_denuncia=:udata OR assunto_denuncia=:uassunto");
+			$atualiza->execute(array(':udata'=>$udata, ':uassunto'=>$uassunto));
 			$row=$atualiza->fetch(PDO::FETCH_ASSOC);
-
-			if($auth_denuncia->register($udata, $uassunto, $udescricao)){
-				echo "Denúncia Gravada.";
-			}
 			
 		}
 		catch(PDOException $e)
@@ -90,24 +83,108 @@
   <![endif]-->
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
-
-<form method="post">
 <!-- Site wrapper -->
-<div>
+<div class="wrapper">
 
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="../../index2.php" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>V</b>C</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>Vc</b>Prefeito</span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <img src="dist/img/avatar5.png" class="user-image" alt="User Image">
+              &nbsp;<?php echo $userRow['user_name']; ?>&nbsp;<span class="caret"></span></a>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- User image -->
+              <li class="user-header">
+                <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
+                <p>
+                  &nbsp;<?php echo $userRow['user_name'];?>
+                  <small>Membro desde 2016</small>
+                </p>
+              </li>
+              
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="#" class="btn btn-default btn-flat">Perfil</a>
+                </div>
+                <div class="pull-right">
+                  <a href="#" class="btn btn-default btn-flat">Sair</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+<!-- Config da barra lateral-->
+
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="../../dist/img/avatar5.png" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p> &nbsp;<?php echo $userRow['user_name']; ?></p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+      </form>
+      <!-- /.search form -->
+      <!-- BARRA LATERAL -->
+      <ul class="sidebar-menu">
+        <li class="header">MENU</li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Denúncias</span> <i class="fa fa-angle-left pull-right"></i>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="denuncias.php"><i class="fa fa-circle-o"></i> Realizar Denúncia</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Consultar</a></li>
+          </ul>
+        </li>
+      </ul>
+      </section>
+      </aside>
 <!-- Fim barra lateral-->
 
   <!-- Content Wrapper. Contains page content -->
-  <div class="content">
+  <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-    
       <h1>
         Denúncias
       </h1>
       
     </section>
-<div class="col-md-12">
+
     <!-- Main content -->
     <section class="content">
 
@@ -123,7 +200,7 @@
           
            <div class="form-group">
             <div class="col-xs-2"> 
-      	  		<input type="date" class="form-control" name="data" placeholder="Data" value="<?php if(isset($error)){echo $udata;}?>"/>
+      	  		<input type="date" class="form-control" name="data" placeholder="Data" />
        	 	</div>
        	  </div>
 
@@ -131,7 +208,7 @@
 			<br />
        	  <div class="form-group">
        	  	<div class="col-xs-6"> 
-      	  		<p align="center"><input type="text" class="form-control" name="assunto" placeholder="Assunto" value="<?php if(isset($error)){echo $uassunto;}?>" /></p>
+      	  		<p align="center"><input type="text" class="form-control" name="assunto" placeholder="Assunto" /></p>
        	 	</div>
        	  </div>
 
@@ -140,16 +217,20 @@
 
        	  <div class="form-group">
        	 	 <div class="col-xs-6"> 
-      	  		<input type="text" class="form-control" name="descricao" placeholder="Descrição" value="<?php if(isset($error)){echo $udescricao;}?>"/>
+      	  		<input type="text" class="form-control" name="descricao" placeholder="Descrição" />
        	 	</div>
        	  </div>
 
           <br />
           <div class="row">
-        
+          <!--<div class="col-xs-12 text-center">
+          <button type="button" class="btn btn-default btn-lrg ajax" title="Ajax Request">
+            <i class="fa fa-spin fa-refresh"></i>&nbsp; Salvar
+          </button>
+          </div>-->
           <br/>
           <div class="col-xs-12 text-center">
-          <button type="submit" class="btn btn-default btn-lrg" name="btn-salvar">
+          <button type="submit" class="btn btn-default btn-lrg" title="btn-salvar">
           <i class="glyphicon glyphicon-ok"></i>&nbsp; Salvar
           </button>
 
@@ -165,7 +246,7 @@
         </div>
         <!-- /.box-body -->
         <div class="box-footer">
-          
+          VcPrefeito.com.br
         </div>
         <!-- /.box-footer-->
       </div>
@@ -173,14 +254,18 @@
 
     </section>
     <!-- /.content -->
-    </div>
   </div>
   <!-- /.content-wrapper -->
 
-  
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      
+    </div>
+    <strong>Copyright &copy;2016 <a href="http://vcprefeito.com.br">VcPrefeito</a>.</strong> Todos os direitos
+    reservados.
+  </footer>
 
 </div>
-</form>
 <!-- ./wrapper -->
 
 <!-- jQuery 2.2.0 -->
@@ -206,7 +291,6 @@
             $('.ajax-content').html('<hr>Ajax Request Completed !');
         }});
     });
-
 </script>
 </body>
 </html>
