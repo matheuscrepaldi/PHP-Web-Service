a7e4c30672d8edd57a32c85fe54d4a8d1ba1ebc4
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-08-14 01:35:36 -0300

    teste fb

--- index2.php
+++ index2.php
@@ -58,23 +58,7 @@
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1120295274701582',
-      xfbml      : true,
-      version    : 'v2.7'
-    });
-  };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
 
 <div class="wrapper">
 
