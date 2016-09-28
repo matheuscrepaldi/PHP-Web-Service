8e1af98a42e6faebb0210eefdcf50b3c202b4bb7
SysAdminVCPref <administrador@vcprefeito.com.br>
2016-09-28 14:47:25 -0300

    relatorio

--- geraRelatorio.php
+++ geraRelatorio.php
@@ -1,3 +1,16 @@
+<?php
+
+    $pdo = new PDO('mysql:host=localhost;dbname=u633448963_login', 'root', '');
+    // define para que o PDO lance exceções caso ocorra erros
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $consulta = $pdo->query("SELECT data_den, desc_den, cat_den, rua_den, cidade_den FROM denuncias;");
+
+    $linha = $consulta->fetchAll(PDO::FETCH_ASSOC);
+   
+
+?>
+
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
 
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js">
@@ -11,7 +24,7 @@
 } );
     
     
-</script>    
+</script>  
     
 
 
@@ -38,12 +51,14 @@
         </tfoot>
         <tbody>
             <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011/04/25</td>
-                
+                <?php while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+                    <td><?php echo $linha['desc_den']?></td>
+                    <td><?php echo $linha['cat_den']?></td>
+                    <td><?php echo $linha['rua_den']?></td>
+                    <td><?php echo $linha['cidade_den']?></td>
+                    <td><?php echo $linha['data_den']?></td>
+                    }
+                ?>           
             </tr>
         </tbody>
     </table>
