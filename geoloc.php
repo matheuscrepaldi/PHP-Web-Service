27d48f5f115012f52a20ee9a3f808f8aca4ab203
vcprefeito <contato@vcprefeito.com.br>
2016-11-16 20:32:35 -0200

    consultar denuncia pelo mapa

--- geoloc.php
+++ geoloc.php
@@ -8,6 +8,45 @@
     <title>Geolocation</title>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
+      <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  
+      <script src="plugins/jQuery/jQuery-2.2.0.min.js"></script>
+
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+<script src="plugins/datatables/jquery.dataTables.min.js"></script>
+
+<script src="plugins/fastclick/fastclick.js"></script>
+
+<script src="dist/js/app.min.js"></script>
+
+<script src="plugins/sparkline/jquery.sparkline.min.js"></script>
+
+<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+
+<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
+
+<script src="plugins/chartjs/Chart.min.js"></script>
+
+<script src="dist/js/demo.js"></script>
+<script src="bootbox.min.js"></script>
+<script src="plugins/datepicker/bootstrap-datepicker.js"></script>
+<script src="plugins/datepicker/locales/bootstrap-datepicker.pt-BR.js"></script>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+
+  <link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+
+  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+      
+      
     <style>
       html, body {
         height: 100%;
@@ -23,6 +62,40 @@
     <div id="map"></div>
 
     <script>
+        
+    function abrirDenuncia(id){
+  
+        $.ajax({
+          type: "POST",
+          url: 'controller/controller_denuncias.php',
+          dataType: 'json',    
+          data: {operacao : "ConsultaDenuncia", denuncia : id},
+          success: function (response) {
+
+            console.log(response);
+             var denuncia = response.data[0];
+             var status = 'Não Resolvida';
+             var imgs = '';
+             var caminho_img = '';
+              
+             if(denuncia.status_den == 'F') status = 'Resolvida';
+
+             $.each(response.img, function (index, value) {
+                //console.log(value);
+                 caminho_img = value.deni_img.replace("../", "");
+                imgs += '<a href="'+ caminho_img +'" target="_blank"><img id="output" src="'+ caminho_img +'" alt="" class="img-rounded" style=" max-width: 160px; max-height: 160px; border: none;"/></a>&nbsp;&nbsp;';
+
+             });
+             
+             
+            bootbox.alert({ message: '<form id="userForm" method="post" class="form-horizontal"> <div class="form-group"><label class="col-xs-4 control-label">Data: </label>  <div class="col-xs-2"><input type="text" class="form-control" name="id" disabled="disabled" value="'+ denuncia.data_den +'" /></div><label class="col-xs-1 control-label">Status: </label><div class="col-xs-3"><input type="text" class="form-control" name="id" disabled="disabled" value="'+ status +'" /></div></div><div class="form-group"><label class="col-xs-4 control-label">Categoria: </label><div class="col-xs-6"><input type="text" class="form-control" name="name" value="'+ denuncia.desc_categoria +'"  disabled="disabled" /></div></div><div class="form-group"><label class="col-xs-4 control-label">Descrição: </label><div class="col-xs-6"><textarea class="form-control" rows="2" id="comment" disabled="disabled" style="resize: none;">'+ denuncia.desc_den +'</textarea></div> </div><div class="form-group"><label class="col-xs-4 control-label">Localização: </label><div class="col-xs-6"><input type="text" class="form-control" name="website" value="'+ denuncia.rua_den + ' / ' + denuncia.cidade_den +'"  disabled="disabled" /></div></div><div class="form-group"><div class="col-xs-2"></div><div class="col-xs-8"><div class="imagem" align="center">'+imgs+'</div></div><div class="col-xs-2"></div></div></form>',
+              title: "Denúncia: " + id,               
+              size: 'large',
+              backdrop: true
+            });
+        }
+        });
+      }
         
         
     var labels = '';
@@ -72,7 +145,7 @@ function initMap() {
         
         
         
-    function addMarker(location) {
+    function addMarker(location, id) {
         var marker = new google.maps.Marker({
             position: location,
             label: labels[labelIndex++ % labels.length],
@@ -81,8 +154,8 @@ function initMap() {
         });
         
         marker.addListener('click', function() {
-            window.location.assign("view/relatorio.php");
-          
+            //window.location.assign("view/relatorio.php");
+            abrirDenuncia(id);
         });
     }
         
@@ -105,8 +178,9 @@ function handleLocationError(browserHasGeolocation, marker, pos) {
 
                 foreach($resultado as $result){ ?>
                    var latlng = {lat:  <?php echo $result['latitude']; ?>, lng: <?php echo $result['longitude']; ?>};
-                 
-                    addMarker(latlng);
+                    var id_den = "<?php echo $result['id_den']; ?>";
+                    
+                    addMarker(latlng, id_den);
             
             <?php    }
             ?>
